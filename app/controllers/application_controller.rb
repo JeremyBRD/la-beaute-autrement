eb03ee0f78233f18eeaab83eb2b99712e13bf913
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-07-26 22:17:11 +0200

    changes banner add social links and footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :copiright_year_update
   # before_action :authenticate_user!
   private
   require 'date'
   def copiright_year_update
-    DateTime.now.year
+    @date_footer = DateTime.now.year
   end
 end
