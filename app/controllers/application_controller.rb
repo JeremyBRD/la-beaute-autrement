119eb48c1969427213272bc983d3096f7f954582
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-07-27 12:55:57 +0200

    basic routins, skeletton of the web app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::Base
   helper_method :copiright_year_update
   # before_action :authenticate_user!
   private
+
   require 'date'
+
   def copiright_year_update
     @date_footer = DateTime.now.year
+  end
+
+  def background_banner
+    DateTime.now.month.between?(4, 9) ? 'background1' : 'background2'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  get 'products_index', to: 'products#index'
+  get 'cares_index', to: 'cares#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
