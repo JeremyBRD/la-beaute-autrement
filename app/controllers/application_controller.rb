97ba05c110519721472d85b4c0b1246ce0175cb4
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-08-09 12:46:57 +0200

    update scraping part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   require 'date'
-  # require 'nokogiri'
-  # require 'open-uri'
-  # require 'byebug'
 
   def copiright_year_update
     @date_footer = DateTime.now.year
@@ -15,11 +12,4 @@ class ApplicationController < ActionController::Base
   def background_banner
     DateTime.now.month.between?(4, 9) ? 'background1' : 'background2'
   end
-
-  # def scraper
-  #   url = "https://slowlabeauteautrement.kalendes.com/visit/#/booking"
-  #   unparsed_page = URI.open(url)
-  #   parsed_page = Nokogiri::HTML(unparsed_page)
-  #   # byebug
-  # end
 end
